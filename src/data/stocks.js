8cd567946162ff0e747746b1ba6efda9fa5f4adc
vivektarun <vivektarun1234@gmail.com>
2025-11-17 14:26:01 +0530

    feat: add stock data

---
+++ src/data/stocks.js
@@ -0,0 +1,9 @@
+const initialStocks = [
+  { symbol: 'AAPL', name: 'Apple Inc.', price: 150.0, available: 50 },
+  { symbol: 'GOOG', name: 'Alphabet Inc.', price: 2800.0, available: 10 },
+  { symbol: 'MSFT', name: 'Microsoft Corp.', price: 300.0, available: 30 },
+  { symbol: 'TSLA', name: 'Tesla Inc.', price: 700.0, available: 20 },
+  { symbol: 'AMZN', name: 'Amazon.com Inc.', price: 3300.0, available: 5 },
+]
+
+export default initialStocks
